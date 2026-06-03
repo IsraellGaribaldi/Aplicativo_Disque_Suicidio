bf916904cab92e4cda5c3be839ba737155a923d4
IsraellGaribaldi <164568033+IsraellGaribaldi@users.noreply.github.com>
2026-06-03 16:51:39 -0300

    Create empresa_model.dart

---
+++ lib/models/empresa_model.dart
@@ -0,0 +1,10 @@
+class Empresa {
+  int? id;
+  double? lat;
+  double? long;
+  String? nome;
+  String? email;
+  String? telefone;
+
+  Empresa({this.id, this.lat, this.long, this.nome, this.email, this.telefone});
+}
