3166660372bc2eb4f29072fcc10ca0d7df04aba4
IsraellGaribaldi <164568033+IsraellGaribaldi@users.noreply.github.com>
2026-06-05 14:56:46 -0300

    mais algumas mudanças

--- lib/pages/home/mapa_inicio.dart
+++ lib/pages/home/mapa_inicio.dart
@@ -1,30 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:app_disque_suicidio/pages/autonomo/autonomo_page.dart';
+import 'package:app_disque_suicidio/pages/clinica/clinicas_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const MapPage(),
-    );
-  }
-}
-
-class MapPage extends StatefulWidget {
-  const MapPage({super.key});
-
-  @override
-  State<MapPage> createState() => _MapPageState();
-}
-
+class MapPage extends StatefulWidget { const MapPage({super.key}); @override State<MapPage> createState() => _MapPageState(); }
 class _MapPageState extends State<MapPage> {
+  int _paginaAtual = 0;
 
   late GoogleMapController mapController;
 
@@ -47,35 +28,47 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Início',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Perfil',
-          ),
-        ],
-      ),
-
-      body: GoogleMap(
+    final telas = [
+      GoogleMap(
         onMapCreated: _onMapCreated,
-
         initialCameraPosition: CameraPosition(
           target: _centro,
           zoom: 13,
         ),
-
         markers: _marcadores,
-
         myLocationEnabled: true,
         zoomControlsEnabled: true,
+      ),
+      ClinicaPage(),
+      AutonomoPage(),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Disque Suicídio'),
+      ),
+      body: telas[_paginaAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _paginaAtual,
+        onTap: (index) {
+          setState(() {
+            _paginaAtual = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Mapa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_hospital),
+            label: 'Clínicas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Autônomos',
+          ),
+        ],
       ),
     );
   }
