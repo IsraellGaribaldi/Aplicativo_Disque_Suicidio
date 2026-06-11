d881911f427410dc2ef3d5c36cb64f815a8116bd
IsraellGaribaldi <164568033+IsraellGaribaldi@users.noreply.github.com>
2026-06-11 08:05:31 -0300

    Update clinicas_controller.dart

--- lib/controllers/clinicas_controller.dart
+++ lib/controllers/clinicas_controller.dart
@@ -1,38 +1,10 @@
 import 'package:app_disque_suicidio/banco/clinica_dao.dart';
 import 'package:app_disque_suicidio/models/clinicas.dart';
 
-<<<<<<< HEAD
-class ClinicasController extends ChangeNotifier {
-  List<Clinica> buscarClinicas() {
-    return [
-      Clinica(
-        nome: 'Clínica Vida',
-        endereco: 'Rua A, 123',
-        horario: '08:00 às 18:00',
-        descricao: 'Atendimento psicológico e psiquiátrico.',
-        nota: 4.8,
-        planos: ['Unimed', 'Hapvida', 'Geap'],
-        latitude: -7.1195,
-        longitude: -34.8450,
-      ),
-
-      Clinica(
-        nome: 'Clínica Esperança',
-        endereco: 'Av. Central, 456',
-        horario: '07:00 às 20:00',
-        descricao: 'Especializada em saúde mental.',
-        nota: 4.6,
-        planos: ['Bradesco', 'SulAmérica'],
-        latitude: -7.1200,
-        longitude: -34.8460,
-      ),
-    ];
-=======
 class ClinicasController {
   final _dao = ClinicaDao();
 
   Future<List<Clinica>> listarClinicas() async {
     return await _dao.buscarClinicas();
->>>>>>> b5318fa07caa7d426f5db46eeca302a7fcee7d24
   }
 }
